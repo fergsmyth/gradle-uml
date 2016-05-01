9ea2e2d28eb45eeb959f80133d9a850a19b40c19
Ferg Smyth <fergsmyth@gmail.com>
2016-05-01 15:57:20 +0100

    Adding Input and Outputs for task. Updating .gitignore for swp files

--- plugin/src/main/groovy/com/ferg/DiagramTask.groovy
+++ plugin/src/main/groovy/com/ferg/DiagramTask.groovy
@@ -4,6 +4,9 @@ import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
 class DiagramTask extends DefaultTask {
+    @InputFiles Iterable<File> classpath
+    @OutputDirectory File generatedFileDir = project.file("${project.buildDir}/generated")
+
     String message = 'Starting Generation'
 
     @TaskAction
