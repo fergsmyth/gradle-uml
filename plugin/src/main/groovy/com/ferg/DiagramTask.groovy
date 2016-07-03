555f258236e540058ae3d64ddd74ae07d517fdbe
Aldo <fergsmyth@gmail.com>
2016-07-03 17:29:52 +0100

    Adding gradlew. Updating plugin to read configured directory for a hardcoded annotation, Entity.

--- plugin/src/main/groovy/com/ferg/DiagramTask.groovy
+++ plugin/src/main/groovy/com/ferg/DiagramTask.groovy
@@ -1,29 +1,30 @@
-package com.ferg
+package com.ferg;
 
-import org.gradle.api.DefaultTask
-import org.gradle.api.tasks.TaskAction
-import org.gradle.api.tasks.OutputDirectory
-import org.gradle.api.tasks.InputFiles
+import org.gradle.api.DefaultTask;
+import org.gradle.api.file.FileTree;
+import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.OutputDirectory;
+import org.gradle.api.tasks.InputDirectory;
 import javax.persistence.Entity;
 import org.reflections.Reflections;
 
 class DiagramTask extends DefaultTask {
-    @InputFiles Iterable<File> source
+    @InputDirectory FileTree source
     @OutputDirectory File generatedFileDir = project.file("${project.buildDir}/generated")
 
     String targetLocation;
 
     @TaskAction
     def generate() {
-	print "Run generate task"
-	source.each { File file -> println file.getName()}
+	println "Source size is : ";
+	println source.size();
+	source.each { File file -> print "Printing file name:"; println file.getName();}
 	Reflections reflections = new Reflections("com.ferg");
-
 	print reflections
         for (Class<?> clazz : reflections.getTypesAnnotatedWith(Entity.class)) {
 	    print "Inside class printout"
-            getLogger().info(clazz.toString());
+       	    getLogger().info(clazz.toString());
 	    print clazz
-        }
+       	}
     }
 }
